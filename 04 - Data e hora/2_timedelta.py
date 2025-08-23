e1786cbcbce33c825d5663bba60ae42c398135d7
Wesley Silva <wesleysilvaconceicao@outlook.com>
2025-08-23 13:24:07 -0300

    Feature/Learning about timedelta

--- 04 - Data e hora/2_timedelta.py
+++ 04 - Data e hora/2_timedelta.py
@@ -1,25 +1,25 @@
 from datetime import date, datetime, timedelta
 
-tipo_carro = "M"  # P, M, G
-tempo_pequeno = 30
-tempo_medio = 45
-tempo_grande = 60
+size_car = "M"  # Pequeno, Médio, Grande
+short_time = 30
+middle_time = 45
+large_time = 60
 data_atual = datetime.now()
 
-if tipo_carro == "P":
-    data_estimada = data_atual - timedelta(days=tempo_pequeno)
+if size_car == "P":
+    data_estimada = data_atual - timedelta(days=short_time)
     print(f"O carro chegou: {data_atual} e ficará pronto às {data_estimada}")
-elif tipo_carro == "M":
-    data_estimada = data_atual - timedelta(days=tempo_medio)
+elif size_car == "M":
+    data_estimada = data_atual - timedelta(days=middle_time)
     print(f"O carro chegou: {data_atual} e ficará pronto às {data_estimada}")
 else:
-    data_estimada = data_atual - timedelta(days=tempo_grande)
+    data_estimada = data_atual - timedelta(days=large_time)
     print(f"O carro chegou: {data_atual} e ficará pronto às {data_estimada}")
 
 
 print(date.today() - timedelta(days=1))
 
-resultado = datetime(2023, 7, 25, 10, 19, 20) - timedelta(hours=1)
-print(resultado.time())
+result_time = datetime(2023, 7, 25, 10, 19, 20) - timedelta(hours=1)
+print(result_time.time())
 
 print(datetime.now().date())
